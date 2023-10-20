d9e792efff1118cd76caee711b3270f7360b63ea
Yasch-Gahi <yasch.gahi@myhsba.de>
2023-10-20 12:33:25 +0200

    db is working :)))

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE lottery_admin
+CREATE TABLE hm_admin
 (
     admin_id       INT Auto_Increment NOT NULL,
     admin_name     varchar(50)        NOT NULL,
@@ -6,12 +6,12 @@ CREATE TABLE lottery_admin
     CONSTRAINT admin_pkey PRIMARY KEY (admin_id)
 );
 
-CREATE TABLE user
+create table hm_user
 (
-    user_id   INT Auto_increment NOT NULL,
-    user_name varchar(50)        NOT NULL,
-    email     varchar(50)        NOT NULL,
-    CONSTRAINT user_pkey PRIMARY KEY (user_id)
+    hm_user_id  INT AUTO_INCREMENT NOT NULL,
+    hm_username varchar(50)        NOT NULL,
+    email       varchar(50)        NOT NULL,
+    CONSTRAINT hm_user_pkey PRIMARY KEY (hm_user_id)
 );
 
 CREATE TABLE event_type
@@ -23,7 +23,7 @@ CREATE TABLE event_type
 
 CREATE TABLE event_hsv
 (
-    eventhsv_id   integer                NOT NULL,
+    event_hsv_id  integer                NOT NULL,
     event_type_id integer                NOT NULL,
     admin_id      integer                NOT NULL,
     opposite      varchar(50)            NOT NULL,
@@ -34,22 +34,21 @@ CREATE TABLE event_hsv
     picture       varchar                NOT NULL,
     deadline      time with time zone    NOT NULL,
     ticket_amount integer                NOT NULL,
-    CONSTRAINT event_hsv_pkey PRIMARY KEY (eventhsv_id),
+    CONSTRAINT event_hsv_pkey PRIMARY KEY (event_hsv_id),
     FOREIGN KEY (event_type_id) REFERENCES event_type (event_type_id),
-    FOREIGN KEY (admin_id) REFERENCES lottery_admin (admin_id)
+    FOREIGN KEY (admin_id) REFERENCES hm_admin (admin_id)
 );
 
-CREATE TABLE lottery_attendance
+CREATE TABLE hm_attendance
 (
-    user_id       integer     NOT NULL,
-    eventhsv_id   integer     NOT NULL,
-    escortname    varchar(50) NOT NULL,
-    winn          boolean     NOT NULL,
-    substitutewin boolean     NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (user_id),
-    FOREIGN KEY (eventhsv_id) REFERENCES event_hsv (eventhsv_id)
+    user_id           integer     NOT NULL,
+    event_hsv_id      integer     NOT NULL,
+    escort_name       varchar(50) NOT NULL,
+    winner            boolean     NOT NULL,
+    substitute_winner boolean     NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES hm_user (hm_user_id),
+    FOREIGN KEY (event_hsv_id) REFERENCES event_hsv (event_hsv_id)
 );
-
 
 CREATE TABLE blacklist
 (
@@ -58,9 +57,6 @@ CREATE TABLE blacklist
     user_id           integer NOT NULL,
     blacklist_counter integer NOT NULL,
     CONSTRAINT blacklist_pkey PRIMARY KEY (blacklist_id),
-    FOREIGN KEY (event_hsv_id) REFERENCES event_hsv (eventhsv_id),
-    FOREIGN KEY (user_id) REFERENCES user (user_id)
+    FOREIGN KEY (event_hsv_id) REFERENCES event_hsv (event_hsv_id),
+    FOREIGN KEY (user_id) REFERENCES hm_user (hm_user_id)
 );
-
-
-
