eb1bda86f3f2ff5a5cb4bc7047b0a43548885d2f
AnnaTari <anata16@outlook.de>
2023-11-04 17:32:19 +0100

    finished with picture

--- src/main/java/com/hansemerkur/lotteryappbackend/controller/EventController.java
+++ src/main/java/com/hansemerkur/lotteryappbackend/controller/EventController.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hansemerkur.lotteryappbackend.model.Event;
 import com.hansemerkur.lotteryappbackend.service.EventService;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
@@ -34,11 +32,12 @@ public class EventController {
 
     @PostMapping(value = "/addEvent", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public List<Event> addEvent(@RequestPart("event") String eventString, @RequestPart("picture") MultipartFile picture) throws IOException {
+        System.out.println(eventString);
         ObjectMapper mapper = new ObjectMapper();
         Event event = mapper.readValue(eventString, Event.class);
         byte[] pictureBytes = picture.getBytes();
         event.setPicture(pictureBytes);
-        System.out.println(Arrays.toString(event.getPicture()));
+        System.out.println(event);
         return eventService.addEvent(event);
     }
 
