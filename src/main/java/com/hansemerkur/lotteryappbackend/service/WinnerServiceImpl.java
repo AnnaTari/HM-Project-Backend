8b7380090125e7d301b92e62df9d55b88c2af751
Koechel <lara.koechel@myhsba.de>
2023-11-13 11:03:20 +0100

    improving lottery logic in ServiveImpl

--- src/main/java/com/hansemerkur/lotteryappbackend/service/WinnerServiceImpl.java
+++ src/main/java/com/hansemerkur/lotteryappbackend/service/WinnerServiceImpl.java
@@ -6,34 +6,29 @@ import com.hansemerkur.lotteryappbackend.repository.WinnerRepository;
 import org.springframework.stereotype.Service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Service
 public class WinnerServiceImpl implements WinnerService {
 
     private final WinnerRepository winnerRepository;
-
+    Random randomNumberGenerator = new Random(); //instance of random-class
 
     public WinnerServiceImpl(WinnerRepository winnerRepository) {
         this.winnerRepository = winnerRepository;
     }
 
 
-//Logic of generating winners with randomnumbergenerator
-
-    //fetching participants
+    //Logic of generating winners with randomnumbergenerator
     @Override
     public List<Winner> selectWinners(Long eventHsvId) {
-        List<Winner> participants = null;
-        return participants;
-    }
+        List<Winner> participants = winnerRepository.findByEventHsvId(eventHsvId); //get participants
+        List<Winner> winners = new ArrayList<>(); //List to save the winners
+        List<Winner> substituteWinners = new ArrayList<>(); //List to save substitute winners
 
-    //Random number generator is instance of random-class
-        Random randomNumberGenerator = new Random();
-
-        //List to save the winners
-        List<Winner> winners = new ArrayList<>();
 
         //Choose ten random authorized winners
         for (int i = 0; i < 10; i++) {
@@ -42,41 +37,44 @@ public class WinnerServiceImpl implements WinnerService {
                     .collect(Collectors.toList());
 
             if (authorizedParticipants.isEmpty()) {
-                break; // No more authorized participants available
+                break; //stop when no more authorized participants available
+            }
+
+            int index = randomNumberGenerator.nextInt(authorizedParticipants.size()); //Random Index
+            Winner winner = authorizedParticipants.get(index); //Choose Winner
+            winners.add(winner);//Add winner to winner list
+            winner.setBlacklistCounter(3);
+            winnerRepository.save(winner);
+            participants.remove(winner); //Remove winner from Participants list
+        }
+
+        //Decreasing blacklist counter for all non-winning participants
+        for (Winner participant : participants) {
+            if (participant.getBlacklistCounter() > 0) {
+                participant.setBlacklistCounter(participant.getBlacklistCounter() - 1);
+                winnerRepository.save(participant);
             }
         }
-        int index = randomNumberGenerator.nextInt(authorizedParticipants.size()); //Random Index
-        Winner winner = authorizedParticipants.get(index); //Choose Winner
-        winners.add(winner);//Add winner to winner list
-        winner.setBlacklistCounter(3);
-        winnerRepository.save(winner);
-        participants.remove(winner); //Remove winner from list
 
+        //Draw five substitute winners without increasing blacklist counter
 
-    // Decrement blacklist counter for all non-winning participants
-        for(
-     Winner participant : participants)
+        for (int i = 0; i < 5; i++) {
+            List<Winner> authorizedParticipants = participants.stream()
+                    .filter(p -> p.getBlacklistCounter() == 0)
+                    .collect(Collectors.toList());
+            if (authorizedParticipants.isEmpty()) {
+                break;
+            }
 
-    {
-        if (participant.getBlacklistCounter() > 0) {
-            participant.setBlacklistCounter(participant.getBlacklistCounter() - 1);
-            winnerRepository.save(participant);
+            int index = randomNumberGenerator.nextInt(authorizedParticipants.size()); // Random Index
+            Winner substituteWinner = authorizedParticipants.get(index); //Choose substitute winners
+            substituteWinners.add(substituteWinner); //Add substitute winners to the winner list
+            participants.remove(substituteWinner); //Remove substitute winners from participant list
         }
-    }
+
+        // Add substitute winners to the main winners list
+        winners.addAll(substituteWinners);
+
         return winners;
-
-
-
-    //Draw five substitute winners without increasing blacklist counter
-    List<String> substituteWinners = new ArrayList<>();
-    for (int i = 0; i < 5; i++) {
-      List<String> authorizedParticipants = new ArrayList<>(participants);
-      authorizedParticipants.removeAll(winners); //Remove previous winners from the list
-
-      int index = randomNumberGenerator.nextInt(authorizedParticipants.size()); // Random Index
-      String substituteWinnerName = authorizedParticipants.get(index); //Choose substitute winners
-      substituteWinners.add(substituteWinnerName); //Add substitute winners to the winner list
-      participants.remove(index); //Remove substitute winners from participant list
     }
-
-  }
+}
